49e5f4695d01e632e7c38e0d0f2b4ad17718e9be
Darryl Morley <darrylmorley.uk@gmail.com>
2020-12-07 21:24:27 +0000

    Add home page and layout with nav

---
+++ pages/components/Layout.jsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Head>
+        <title>Darryls' Home</title>
+      </Head>
+      <nav>
+        <h1>Darryls' Stuff</h1>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>
+                Home
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/async-hooks">
+              <a>
+                Giffy Async Hooks
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default Layout
